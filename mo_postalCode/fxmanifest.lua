ed9c50adfc006c52d251d996ecc716fd610147d9
Mo1810 <68502762+Mo1810@users.noreply.github.com>
2021-05-06 10:07:06 +0200

    Add files via upload

---
+++ mo_postalCode/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'bodacious'
+game 'gta5' 
+
+author 'Mo1810#4230'
+version '1.0.0'
+
+client_scripts { 
+	"postalCode-c.lua",
+	"config.lua"
+}
+
+server_scripts { 
+	"postalCode-s.lua",
+	"config.lua"
+}
